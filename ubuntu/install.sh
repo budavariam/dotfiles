bc8f17022adefaf505d013d4b649bea108a236f8
Mátyás Budavári <budavariam@gmail.com>
2018-02-25 17:19:10 +0100

    Fix chrome bug, only opens blank pages in unity from external source

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,6 +4,7 @@
 # If something is not working the installation method might have changed since.
 
 # ack-grep
+apt-get install vim
 apt-get install ack-grep
 apt-get install wget
 apt-get install curl
@@ -26,3 +27,14 @@ echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sud
 #Install package
 sudo apt-get update
 sudo apt-get install google-chrome-stable
+
+# In ubuntu1604unity google-chrome can not open links from external applications
+
+vim -es -c '%s/^\(Exec=\/opt\/google\/chrome\/chrome\)$/\1 %U/g' -c wq "$HOME/.local/share/applications/google-chrome.desktop"
+
+# As unfulvio said, the issue is with google-chrome.desktop, and it is missing the %U argument .
+# Open file: "$HOME/.local/share/applications/google-chrome.desktop"
+# Find the line: "Exec=/opt/google/chrome/chrome"
+# Add a space and %U: "Exec=/opt/google/chrome/chrome %U"
+# Source: https://askubuntu.com/a/701775/785947
+# I have automated it with vim ex mode. Regex match the line and replace it with the added extra 3 characters. Note that the capture group parenthesis has to be escaped.
