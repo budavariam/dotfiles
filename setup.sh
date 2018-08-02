6359f02283831d0b55a3a759dd2a2714b272259d
Mátyás Budavári <budavariam@gmail.com>
2018-08-02 18:08:42 +0200

    Add backups for already existing files

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 
 DOTFILES_DIRECTORY="`pwd`"
 VSCODE="${HOME}/.config/Code/User"
+TEMP="${HOME}/.tmpdotfiles"
 
 linkhome() {
+    cp "${HOME}/${2}" "$TEMP" 2>/dev/null
     # Force create/replace the symlink.
     ln -fs "${DOTFILES_DIRECTORY}/${1}" "${HOME}/${2}"
 }
@@ -13,7 +15,8 @@ linkvscode(){
     ln -fs "${DOTFILES_DIRECTORY}/${1}" "${VSCODE}/${2}"
 }
 
-printf "Create Symlinks\n"
+printf "Create Symlinks with backup.\n"
+mkdir -p $TEMP
 # Create the necessary symbolic links between the `.dotfiles` and the appropriate directory.
 # The `bash_profile` sources other files directly from the `dotfiles` repository.
 linkhome "shell/.bashrc"             ".bashrc"
