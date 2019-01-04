aebf4ca6b36148002982ed799ee609bba380d69c
Mátyás Budavári <budavariam@gmail.com>
2019-01-04 17:50:29 +0100

    Add zsh to macOS, reorganize installs

--- _mac/install.sh
+++ _mac/install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Run this script on a fresh mac to get some useful tools
+# Run this script line by line on a fresh mac to get some useful tools.
 
 #install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# wget
 brew install wget
+# ack-grep
+brew install ack
+# tree
+brew install tree
+# midnight commander
+brew install mc
 
 # install cask to install other programs
 brew tap caskroom/cask
-
 # kdiff3
 brew cask install kdiff3
 # copyq
@@ -18,7 +24,8 @@ brew cask install copyq
 brew cask install google-chrome
 # meld
 brew cask install meld
-# ack-grep
-brew install ack
-# tree
-brew install tree
+# iterm 2
+brew cask install iterm2
+
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
