5598d4e15138699beec63a0ddd6f5e4d0e31f1d9
Mátyás Budavári <budavariam@gmail.com>
2025-02-11 12:13:24 +0100

    feat(sketchybar): :feat: color battery state

--- _mac/sketchybar/.config/sketchybar/plugins/battery.sh
+++ _mac/sketchybar/.config/sketchybar/plugins/battery.sh
@@ -3,20 +3,36 @@
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
+COLOR_GREEN="0xaa00FF00"
+COLOR_DEFAULT="0xaaffffff"
+COLOR_RED="0xffFF0000"
+COLOR="0xaaffffff"
+
 if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+  9[0-9]|100)
+    ICON=""
+    COLOR="$COLOR_GREEN"
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9])
+    ICON=""
+    COLOR="${COLOR_DEFAULT}"
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9])
+    ICON=""
+    COLOR="${COLOR_DEFAULT}"
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9])
+    ICON=""
+    COLOR="${COLOR_DEFAULT}"
   ;;
-  *) ICON=""
+  *)
+    ICON=""
+    COLOR="${COLOR_RED}"
+  ;;
 esac
 
 if [[ "$CHARGING" != "" ]]; then
@@ -25,4 +41,4 @@ fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" label.color="${COLOR}"
