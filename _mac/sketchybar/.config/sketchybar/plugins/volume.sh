a95c1a69bb64ffdb32eddcb872632be0224a7db5
Mátyás Budavári <budavariam@gmail.com>
2026-06-25 16:50:06 +0200

    fix: os update volume event

--- _mac/sketchybar/.config/sketchybar/plugins/volume.sh
+++ _mac/sketchybar/.config/sketchybar/plugins/volume.sh
@@ -5,16 +5,18 @@
 
 if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
-
-  case "$VOLUME" in
-    [6-9][0-9]|100) ICON="󰕾"
-    ;;
-    [3-5][0-9]) ICON="󰖀"
-    ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿"
-    ;;
-    *) ICON="󰖁"
-  esac
-
-  sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
+else
+  VOLUME=$(osascript -e "output volume of (get volume settings)")
 fi
+
+case "$VOLUME" in
+  [6-9][0-9]|100) ICON="󰕾"
+  ;;
+  [3-5][0-9]) ICON="󰖀"
+  ;;
+  [1-9]|[1-2][0-9]) ICON="󰕿"
+  ;;
+  *) ICON="󰖁"
+esac
+
+sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
